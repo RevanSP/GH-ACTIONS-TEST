26ce4752a8069b73735fa79d76d4bbe5fe803483
RevanSP <revanspstudy28@gmail.com>
2025-04-17 14:55:41 +0700

    idk:

--- scrape.js
+++ scrape.js
@@ -1,20 +1,20 @@
-const puppeteer = require('puppeteer-core');
+const puppeteer = require('puppeteer');
 const fs = require('fs');
-const { executablePath } = require('puppeteer');
 
 (async () => {
   const browser = await puppeteer.launch({
-    executablePath: executablePath(),
     headless: true,
     args: ['--no-sandbox', '--disable-setuid-sandbox']
   });
 
   const page = await browser.newPage();
-  await page.goto('https://jsonplaceholder.typicode.com/users');
+  await page.goto('https://jsonplaceholder.typicode.com/users', { waitUntil: 'networkidle2' });
 
   const body = await page.evaluate(() => document.body.innerText);
   const data = JSON.parse(body);
 
+  fs.mkdirSync('public', { recursive: true });
   fs.writeFileSync('public/users.json', JSON.stringify(data, null, 2));
+
   await browser.close();
 })();
