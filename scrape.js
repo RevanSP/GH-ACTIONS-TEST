57f51d8684fa8be95fb1c3a850934da00cdce7e7
RevanSP <revanspstudy28@gmail.com>
2025-04-17 15:05:53 +0700

    lol

--- scrape.js
+++ scrape.js
@@ -3,18 +3,17 @@ const fs = require('fs');
 
 (async () => {
   const browser = await puppeteer.launch({
-    headless: true, // Pastikan mode headless
+    headless: true,  // Jalankan browser dalam mode headless
+    args: ['--no-sandbox', '--disable-setuid-sandbox'],  // Menonaktifkan sandboxing
   });
 
   const page = await browser.newPage();
   await page.goto('https://jsonplaceholder.typicode.com/users');
 
-  // Ambil data dalam format JSON
   const data = await page.evaluate(() => {
     return JSON.parse(document.body.innerText);
   });
 
-  // Simpan data sebagai file JSON
   fs.writeFileSync('public/users.json', JSON.stringify(data, null, 2));
 
   await browser.close();
